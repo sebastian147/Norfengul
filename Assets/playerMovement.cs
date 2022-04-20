c26099c5916ac118a5188c499305b0bc8249904b
Sebastian Dopazo <sebastiandopazo@outlook.com.ar>
2022-04-20 18:17:45 -0300

    Unifico funciones de player y creo clase mob

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -2,36 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class playerMovement : mob
 {
-    public CharacterController2D controller;
-    public Animator animator;
-    public float runSpeed = 40f;
-
-    float horizontalMove = 0f;
-    bool jump = false;
-
-    void Update()
+    public override void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        base.Update();
 
         if(Input.GetButtonDown("Jump"))
         {
-            jump = true;
-            animator.SetBool("isJumping", true);
-
+            Jump();
         }
-    }
-    public void OnLanding ()
-    {
-        animator.SetBool("isJumping", false);
-        Debug.Log("hola");
-    }
-    void FixedUpdate()
-    {
-        //move or character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
     }
 }
