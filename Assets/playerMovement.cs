e83e177c54fc168b419e6bb51ffd7fe72c97cb8b
Sebastian Dopazo <sebastiandopazo@outlook.com.ar>
2022-04-21 05:07:15 -0300

    arreglo online

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class playerMovement : mob
 {
+    PhotonView view;
+    public void Start() {
+        view = GetComponent<PhotonView>();
+    }
     public override void Update()
     {
-        base.Update();
+        if(view.IsMine)
+        {
+            base.Update();
 
-        if(Input.GetButtonDown("Jump"))
-        {
-            Jump();
-        }
-        if(Input.GetMouseButtonDown(0))
-        {
-            Attack();
+            if(Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+            if(Input.GetMouseButtonDown(0))
+            {
+                Attack();
+            }
         }
     }
 }
