1b5bfda93e1689e5fb13bb31735a883114659223
Sebastian Dopazo <sebastiandopazo@outlook.com.ar>
2022-04-21 04:17:07 -0300

    agrego online

---
+++ Assets/CreateAndJoinsRooms.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
+
+public class CreateAndJoinsRooms : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+}
