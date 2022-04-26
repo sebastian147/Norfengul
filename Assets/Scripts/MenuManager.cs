7b15bebac43aea10e5634cda47476f893d3afa4e
SantiagoDopazo <104122457+SantiagoDopazo@users.noreply.github.com>
2022-04-26 03:47:57 -0300

    Arego Menu de Inicio

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] Menu[] menus;
+
+    public void OpenMenu(string menuName)
+    {
+
+    }
+
+    public void OpenMenu(Menu menu)
+    {
+        
+    }
+}
