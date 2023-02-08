39514c042c1769d8e354c56a7fde9cf0d093898d
Sebastian Dopazo <sebastiandopazo@outlook.com.ar>
2023-02-08 17:36:34 -0300

    arreglo errores del merge

--- Assets/Scripts/Items/Guns/Arma.cs
+++ Assets/Scripts/Items/Guns/Arma.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Armas : MonoBehaviour
+public class Arma : MonoBehaviour
 {
         public Animator anim;
         // Start is called before the first frame update
