b9b0ee293ffeb7827f6d836f7b0e4f570c492aa4
Sebastian Dopazo <sebastiandopazo@outlook.com.ar>
2023-02-12 19:00:24 -0300

    arreglo bug de correr contra la pared

--- Assets/Scripts/Mobs/objects_aux/States/RunningState.cs
+++ Assets/Scripts/Mobs/objects_aux/States/RunningState.cs
@@ -35,7 +35,7 @@ public class RunningState : MobBaseState
             myMob.actualState = myMob.myStateMachine.changeState(myStates.Jump,myMob);
             return;
         }
-        if(Mathf.Abs(myMob.myRigidbody.velocity.x) < 1)
+        if(Mathf.Abs(myMob.myRigidbody.velocity.x) < 1 && Mathf.Abs(myMob.horizontalMove) == 0)
         {
             myMob.actualState = myMob.myStateMachine.changeState(myStates.Idle,myMob);
             return;
